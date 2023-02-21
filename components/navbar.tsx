e0da0b6b3a8b2044e8ddc946103df45e4fc4ba80
Jeff Miller <jeffery.f@gmail.com>
2023-02-20 20:54:59 -0600

    Updates Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,43 +1,146 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-import React from 'react'
-import {
-  TopAppBar,
-  TopAppBarRow,
-  TopAppBarSection,
-  TopAppBarFixedAdjust,
-} from 'rmwc'
-import '@material/top-app-bar/dist/mdc.top-app-bar.css'
-import '@material/icon-button/dist/mdc.icon-button.css'
-import '@material/ripple/dist/mdc.ripple.css'
-import '@rmwc/icon/icon.css'
-import '@material/drawer/dist/mdc.drawer.css'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import Container from '@mui/material/Container'
+import { MdMenu } from 'react-icons/md'
+import { GetStaticProps } from 'next'
+import { client } from '../lib/api'
+import { Entry, EntryCollection } from 'contentful'
+import { IGenericPageFields } from '../@types/generated/contentful'
+import { Button, MenuItem } from '@mui/material'
 import Link from 'next/link'
 
+// const pages = ['Products', 'Pricing', 'Blog']
+
 const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null)
+  const [pages, setPages] = React.useState<
+    EntryCollection<IGenericPageFields> | undefined
+  >(undefined)
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // @ts-ignore
+    setAnchorElNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  React.useEffect(() => {
+    // declare the async data fetching function
+    const fetchData = async () => {
+      const pages: EntryCollection<IGenericPageFields> =
+        await client.getEntries({
+          content_type: 'genericPage',
+          'fields.slug[ne]': 'clinics/index',
+        })
+
+      setPages(pages)
+    }
+
+    // call the function
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error)
+  }, [])
+
   return (
-    <>
-      <TopAppBar
-        fixed
-        style={{
-          backgroundColor: '#472B74',
-          padding: '1rem',
-          boxShadow: ' 0px 4px 30px rgba(0, 0, 0, 0.25)',
-        }}
-      >
-        <TopAppBarRow>
-          <TopAppBarSection alignStart>
-            <Link href="/">
-              <img src="/CDPH Loog.png" alt="California Public Health Logo" />
-            </Link>
-          </TopAppBarSection>
-          <TopAppBarSection alignEnd>
-            <img src="/beatit.png" alt="Test It logo" />
-          </TopAppBarSection>
-        </TopAppBarRow>
-      </TopAppBar>
-      <TopAppBarFixedAdjust />
-    </>
+    <AppBar
+      position="fixed"
+      sx={{
+        backgroundColor: '#472B74',
+        boxShadow: ' 0px 4px 30px rgba(0, 0, 0, 0.25)',
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: 'flex' }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              <img src="/beatit.png" alt="Test It logo" />
+            </Typography>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages
+                ? pages.items.map((page) => (
+                    <Button
+                      key={page.sys.id}
+                      onClick={handleCloseNavMenu}
+                      sx={{ my: 2, color: 'white', display: 'block' }}
+                      href={`/${page.fields.slug}`}
+                    >
+                      {page.fields.pageTitle}
+                    </Button>
+                  ))
+                : null}
+            </Box>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MdMenu />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages
+                  ? pages.items.map((page) => (
+                      <MenuItem key={page.sys.id} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                          <Link href={`/${page.fields.slug}`}>
+                            {page.fields.pageTitle}
+                          </Link>
+                        </Typography>
+                      </MenuItem>
+                    ))
+                  : null}
+              </Menu>
+            </Box>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
-
 export default Navbar
